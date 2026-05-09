e32d7f8f529cd2cac4ccf786959365c45a955900
ros_2 <ros_2@ros2.myguest.virtualbox.org>
2026-05-09 12:29:45 +0300

    This commmit was made after the sample base for the task manager and web bridge along with their integration to the setup.py and package.xml was completed

---
+++ src/my_robot_store/my_robot_store/web_bridge.py
@@ -0,0 +1,56 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from flask import Flask, render_template_string
+import threading
+
+class WebBridge (Node):
+    def __init__(self):
+        super().__init__('web_bridge_node')
+        self.publisher_ = self.create_publisher(String, 'customer_order', 10)
+    
+    def send_order(self, items):
+        msg = String()
+        msg.data = items
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Published Order to ROS2: {items}')
+
+
+app = Flask(__name__)
+ros_node = None
+
+HTML_PAGE = """
+<html>
+    <body>
+        <h1></h1>
+        <form action="/order" method="post">
+            <button name="items" value="Milk, Bread, Eggs" type="submit" style="padding:20px; font-size:20px">
+                Order: Milk, Bread, Eggs
+            </button>
+        </form>
+    </body>
+</html>
+"""
+
+app.route("/")
+def home():
+    return render_template_string(HTML_PAGE)
+
+app.route("/order", methods=['POST'])
+def order():
+    ros_node.send_order("Milk, Bread, Eggs")
+    return "<h1> Order Sent to Robot!<h1><a href='/'>Go Back</a>"
+
+def main():
+    global ros_node
+    rclpy.init()
+    ros_node = WebBridge()
+
+    threading.Thread(target=lambda: app.run(host='0.0.0.0', port=5000, debugg=False, use_reloader=False)).start()
+
+    rclpy.spin(ros_node)
+    ros_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
---
+++ src/my_robot_store/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_robot_store'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros_2',
+    maintainer_email='ros_2@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'manager = my_robot_store.task_manager:main',
+            'web_app = my_robot_store.web_bridge:main',
+        ],
+    },
+)
