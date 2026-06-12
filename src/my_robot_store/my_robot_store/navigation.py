69346736d1d1516b60a182b0a54381c06da5e15a
ros_2 <ros_2@ros2.myguest.virtualbox.org>
2026-06-12 15:30:48 +0300

    This commit was made after the visual simulation of the bot was added in

--- src/my_robot_store/my_robot_store/navigation.py
+++ src/my_robot_store/my_robot_store/navigation.py
@@ -1,7 +1,8 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, TransformStamped
 from std_msgs.msg import String
+import tf2_ros
 
 class Nav(Node):
     def __init__(self):
@@ -12,6 +13,8 @@ class Nav(Node):
         self.pos_pub = self.create_publisher(Point, 'robot_position', 10)
 
         self.status_pub = self.create_publisher(String, 'nav_status', 10)
+
+        self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
 
         self.current_pos = [0.0, 0.0]
         self.target_pos = [0.0, 0.0]
@@ -39,26 +42,41 @@ class Nav(Node):
             self.get_logger().warn(f"Location coordinates unknown for item: {item}")
 
     def move_bot(self):
-        if not self.is_moving:
-            return
-        
         for i in range(2):
-            diff = self.target_pos[i] - self.current_pos[i]
-            if abs(diff) > 0.05:
-                self.current_pos[i] += 0.1 if diff > 0 else -0.1
+            if self.is_moving:
+                diff = self.target_pos[i] - self.current_pos[i]
+                if abs(diff) > 0.05:
+                    self.current_pos[i] += 0.1 if diff > 0 else -0.1
 
         p = Point()
         p.x, p.y, p.z = self.current_pos[0], self.current_pos[1], 0.0
         self.pos_pub.publish(p)
 
-        dist = abs(self.current_pos[0] - self.target_pos[0]) + abs(self.current_pos[1] - self.target_pos[1])
-        if dist < 0.15:
-            self.is_moving = False
-            self.get_logger().info("Arrived at destination!")
+        t = TransformStamped()
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'map'
+        t.child_frame_id = 'base_link'
 
-            status_msg = String()
-            status_msg.data = "ARRIVED"
-            self.status_pub.publish(status_msg)
+        t.transform.translation.x = self.current_pos[0]
+        t.transform.translation.y = self.current_pos[1]
+        t.transform.translation.z = 0.0
+
+        t.transform.rotation.x = 0.0
+        t.transform.rotation.y = 0.0
+        t.transform.rotation.z = 0.0
+        t.transform.rotation.w = 1.0
+
+        self.tf_broadcaster.sendTransform(t)
+
+        if self.is_moving:
+            dist = abs(self.current_pos[0] - self.target_pos[0]) + abs(self.current_pos[1] - self.target_pos[1])
+            if dist < 0.15:
+                self.is_moving = False
+                self.get_logger().info("Arrived at destination!")
+
+                status_msg = String()
+                status_msg.data = "ARRIVED"
+                self.status_pub.publish(status_msg)
 
 def main():
     rclpy.init()
